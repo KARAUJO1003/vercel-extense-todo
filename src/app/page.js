8b1f8a286ce6c6eafb82b8c6d37f618af1b93e03
KARAUJO1003 <129511975+KARAUJO1003@users.noreply.github.com>
2023-12-27 12:42:50 -0300

    Adicionando tarefa com botão Enter

--- src/app/page.js
+++ src/app/page.js
@@ -7,16 +7,29 @@ import { Separator } from '@/components/ui/separator'
 import { Tabs, TabsTrigger, TabsContent, TabsList } from '@/components/ui/tabs'
 import { useState } from 'react'
 import { v4 } from 'uuid'
+import Image from 'next/image'
+import * as bg from '@/app/assets/bg-violet.jpg'
 
 export default function Home() {
 
-  const [ valueInput, setValueInput ] = useState('')
-  const [ listTasks, setListTasks ] = useState([])
-  const [ IsChecked, setIsChecked ] = useState(false)
+  const [valueInput, setValueInput] = useState('')
+  const [listTasks, setListTasks] = useState([])
+  const [IsChecked, setIsChecked] = useState(false)
 
   function HandleNewTask() {
-    setListTasks([{ id: v4(), name: valueInput, checked: IsChecked }, ...listTasks])
-    console.log(listTasks)
+    if (!valueInput) {
+      alert('digite algo para continuar')
+    } else {
+      setListTasks([{ id: v4(), name: valueInput, checked: IsChecked }, ...listTasks])
+      setValueInput('')
+    }
+
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      HandleNewTask();
+    }
   }
 
   function HandleCheck(id) {
@@ -26,17 +39,21 @@ export default function Home() {
       );
     });
   }
-
   return (
     <main className="flex min-h-screen flex-col">
-      <div className='h-[200px] w-full bg-zinc-100 flex items-center justify-center'>
-        <h1>background image</h1>
+      <div className='h-[200px] w-full overflow-hidden bg-zinc-100 flex items-center justify-center'>
+        <Image
+          className='bg-cover z-0'
+          sizes='(max-width: 1560px) 100vw, (max-width: 1200px) 50vw, 33vw'
+          src={bg}
+          alt='pictre'
+        />
       </div>
       <Separator />
       <Tabs
         defaultValue='todolist'
       >
-        <TabsList className='w-full flex justify-center bg-zinc-200 rounded-none px-10'>
+        <TabsList className='w-full flex justify-center  rounded-none px-10'>
           <TabsTrigger value="todolist">Lista de Tarefas</TabsTrigger>
           <TabsTrigger value="conversor">Conversor de números</TabsTrigger>
         </TabsList>
@@ -48,6 +65,7 @@ export default function Home() {
             <Input
               onChange={(e) => setValueInput(e.target.value)}
               placeholder="Digite uma nova tarefa..."
+              onKeyDown={handleKeyDown}
             />
             <Button
               onClick={HandleNewTask}
@@ -56,16 +74,16 @@ export default function Home() {
             </Button>
           </div>
           <ul className='mt-7'>
-            {listTasks.map((item) =>  (
+            {listTasks.map((item) => (
               <li
-                className='flex items-center gap-3' 
+                className='flex items-center gap-3'
                 key={item.id}
               >
                 <Checkbox
                   checked={item.checked}
                   onCheckedChange={() => HandleCheck(item.id)}
                 />
-                <span className={`${item.checked ? "line-through text-zinc-500" : "text-zinc-900" }`}>{item.name}</span>
+                <span className={`${item.checked ? "line-through text-zinc-500" : "text-zinc-900"}`}>{item.name}</span>
               </li>
             ))}
           </ul>
